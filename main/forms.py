ffc3244943289a176db64a8726b652d8ae60d3bf
OkehBero <davetjoeng854@gmail.com>
2026-09-21 22:00:49 +0700

    membuat ExperienceForm

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,7 @@
-from django.forms import ModelForm, TextInput, Textarea, URLInput
+from django.forms import ModelForm, TextInput, Textarea, URLInput, Select
+from django import forms
 
-from main.models import Project
+from main.models import Project, Experience
 
 class ProjectForm(ModelForm):
     class Meta:
@@ -48,5 +49,34 @@ class ProjectForm(ModelForm):
                 attrs={
                     "placeholder": "https://drive.google.com/thumbnail?id=...&sz=w1000",
                 }
+            ),
+        }
+        
+class ExperienceForm(ModelForm):
+    class Meta:
+        model = Experience
+        fields = ["title", "description", "category", "thumbnail", "ended_at"]
+        labels = {
+            "title": "Posisi / Judul Pengalaman",
+            "description": "Deskripsi Pengalaman",
+            "category": "Kategori Pengalaman",
+            "thumbnail": "URL Thumbnail (Opsional)",
+            "ended_at": "Tanggal Selesai (Kosongkan jika masih berlangsung)",
+        }
+        widgets = {
+            "title": TextInput(
+                attrs={"placeholder": "Asisten Dosen PBP", "maxlength": 255}
+            ),
+            "description": Textarea(
+                attrs={"placeholder": "Jelaskan peran dan tanggung jawabmu...", "rows": 3}
+            ),
+            "category": Select(
+                attrs={"class": "form-select"}
+            ),
+            "thumbnail": URLInput(
+                attrs={"placeholder": "https://drive.google.com/..."}
+            ),
+            "ended_at": forms.DateTimeInput(
+                attrs={"type": "datetime-local"}
             ),
         }
